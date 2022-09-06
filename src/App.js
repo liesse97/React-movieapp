8edb1288bc568597018656330a5336af66a6aa56
Liesse Ishimwe <ishimwelieish@gmail.com>
2022-09-06 20:12:04 +0200

    'fi'

---
+++ src/App.js
@@ -0,0 +1,104 @@
+import React,{useState,useEffect} from 'react';
+import './App.css';
+import FilmCard from './component/FilmCard';
+import {Navbar,Nav,Container,FormControl,Form,Button} from 'react-bootstrap';
+
+function App() {
+  const [film, setFilm] = useState([]);
+  const [movieSearch,setmovieSearch] = useState('');
+
+  useEffect(() => {
+    fetch(process.env.REACT_APP_API_URL)
+    .then((res)=>res.json())
+    .then(data=>{
+      console.log(data);
+      setFilm(data.results)
+  })
+  }, [])
+
+const search = async(e)=>{
+  e.preventDefault();
+  console.log('search')
+  //variable search api
+  // try{
+const urlSearch=`https://api.themoviedb.org/3/search/movie?api_key=222cb242b670484d3d3c617737aec12e&query=${movieSearch}`;
+// const urlSearch=`${process.env.REACT_APP_API_SEARCH}=${movieSearch}`;
+
+
+  //convert url inton json
+  const res= await fetch(urlSearch);
+  const data= await res.json();
+  console.log(data);
+  setFilm(data.results)
+
+// }
+//   catch(e){
+// console.log(e)
+//   }
+
+}
+
+const handler =(e)=>{
+  setmovieSearch(e.target.value)
+
+}
+
+  
+  return (
+    <>
+    <Navbar bg='dark' expand='lg' variant='dark' >
+      <Container fluid style={{
+    fontWeight:'600'}}> 
+      <Navbar.Brand href ='/home'> MovieApp</Navbar.Brand>
+            <Navbar.Brand href ='/home'> Popular</Navbar.Brand>
+            {/* collapsible navbar bar */}
+          <Navbar.Toggle arial-controls='navbarSroll'></Navbar.Toggle>
+ 
+              <Navbar.Collapse id="navbarScroll">
+                <Nav 
+                className="me-auto my-2 my-lg-3" 
+                style={{maxHeight:'110px'}}
+                navbarScroll></Nav>
+ <Form className="d-flex" onSubmit={search}>
+              <FormControl
+              type="search"
+              placeholder="Movie Search"
+              className="me-2"
+              aria-label="search"
+              name="search movies"
+              value={movieSearch} onChange={handler}></FormControl>
+              <Button variant="secondary" type="submit">Search</Button>
+            </Form>
+
+
+              </Navbar.Collapse>
+
+
+
+      </Container>
+
+    </Navbar>
+    <div>
+      { film && film.length > 0 ?(
+<div className="App-cont">
+          <div className="grid-cont">
+      {/* mappping moviesattribute into moviecard components */}
+{film.map((filmAttr)=><FilmCard key={filmAttr.id} {...filmAttr}/>)}  
+    </div>
+     </div>
+    ):(
+      
+                 <h2>Movie Not Found!</h2>
+
+    )}
+    
+        </div>
+</>
+
+  );
+}
+
+export default App;
+
+//  30:54 :https://www.youtube.com/watch?v=KveKdXSEV7A&t=1937s
+// 23:16 : Select Favorite(movies): https://www.youtube.com/watch?v=jc9
---
+++ src/component/FilmCard.jsx
@@ -0,0 +1,51 @@
+import React,{useState} from 'react'
+import{Modal,show,Button} from 'react-bootstrap'
+
+
+const FilmCard = ({title,poster_path,release_date,vote_average,overview}) => {
+ const [show, setShow] = useState(false)
+
+ const handleOpen=()=> setShow(true)
+  const handleClose=()=> setShow(false)
+
+ 
+  return (
+    <div className='card text-center bg-secondary mb-4'>
+        <div className='card-body' style={{
+    backgroundColor:'#212529'}}>
+                    <img className='card-img-top'src={process.env.REACT_APP_API_IMG + poster_path} alt="" />
+ <div className='card-body'>
+    <button type="button" className="btn btn-warning" onClick={handleOpen}>Read more</button>
+<Modal show={show} onHide={handleClose}>
+<Modal.Header closeButton>
+    <Modal.Title>{title}</Modal.Title>
+  </Modal.Header>
+
+  <Modal.Body>
+      <img className='card-img-top' style={{width:'224px'}}src={process.env.REACT_APP_API_IMG +poster_path} alt="" />
+      {/* <h3>{title}</h3> */}
+      {/* <h4>ImDB: {vote_average}</h4>
+      <h5>Release Date:{release_date}</h5> */}
+      {/* <br/> */}
+      <h4>Overview</h4>
+      <p>{overview}</p>
+      <h6>Release Date:{release_date}</h6>
+      <h6>ImDB: {vote_average}</h6>
+        </Modal.Body>
+<Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+
+
+ 
+</Modal>
+ </div>
+        </div>
+    </div>
+
+  )
+}
+
+export default FilmCard
